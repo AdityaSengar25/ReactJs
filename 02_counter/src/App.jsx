62431bce15ca8acf29706fab49d650bbfcd68f22
Aditya Sengar <2k23.csaiml2311761@gmail.com>
2026-03-27 00:25:34 +0530

    Interview Question

--- 02_counter/src/App.jsx
+++ 02_counter/src/App.jsx
@@ -14,6 +14,31 @@ function App() {
     // count=count+1;
     // setCounter(count) // can be set like this too
     setCounter(count+1)
+
+
+    // 👇🏼 here if we want to update the value lets say like this: counter + 4
+    // this doesnt work cause of fibre as if these are all same the whole thing is taken in batch and only one of the is executed 
+
+    // React batches these updates (thanks to Fiber), and since all updates are the same value (1), it effectively results in:
+    // final state = 1
+    // 👉 Not because it ignores them, but because they all resolve to the same value.
+
+    // setCounter(count+1)
+    // setCounter(count+1)
+    // setCounter(count+1)
+    // setCounter(count+1)
+
+    // so if we do want to this kind of thing we do, here we have a callback fn in set method and 
+    // prevCount is not captured once like count.
+    // Instead:
+    // React calls each function later
+    // Each function receives the latest updated state
+    
+    setCounter(prevCount => prevCount + 1)
+    setCounter(prevCount => prevCount + 1)
+    setCounter(prevCount => prevCount + 1)
+    setCounter(prevCount => prevCount + 1)
+
   }
   const removeValue=()=>{
     if(count>0){
