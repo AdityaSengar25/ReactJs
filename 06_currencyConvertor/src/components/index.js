c9589bca9457a71f467747d67e6b8cc2c1fd2d1b
Aditya Sengar <2k23.csaiml2311761@gmail.com>
2026-04-01 20:48:43 +0530

    PasswordGenerator & CurrencyConvertor

---
+++ 06_currencyConvertor/src/components/index.js
@@ -0,0 +1,7 @@
+// good practice to import everycomponent here and then export it to App.jsx
+
+import InputBox from "./InputBox";
+
+export {InputBox}
+
+//export { default as InputBox } from "./InputBox"; // short version to import and export at the same time
